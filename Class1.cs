5a331169fd0a47d98dd3f448459eba81901a0ca2
Anusha98s <92158408+Anusha98s@users.noreply.github.com>
2021-10-14 12:08:28 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Exception_Handling
+{
+    class Class1
+    {
+    }
+    [Serializable]
+    public class StudentNotFoundException : Exception
+    {
+        public string StudentName { get; set; }
+        public StudentNotFoundException() { }
+
+        public StudentNotFoundException(string message) : base(message) {}
+
+        //Console.WriteLine("Again base class method is defined in exception class");
+
+        public StudentNotFoundException(string Message, Exception InnerException) : base(Message, InnerException) { }
+
+        public StudentNotFoundException(string message, string studentName) 
+        :this(message)
+           
+             {
+                      StudentName = studentName;
+    }
+            
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Demo_Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Console.WriteLine("enter your fav No");
+                //string No = Console.ReadLine();
+                //Int32.Parse(No);
+
+                //Console.WriteLine("No you entered {0}. valid Integer32",No);
+                throw new StudentNotFoundException("Student Not Found", "Anusha");
+
+            }
+            catch (StudentNotFoundException s)
+            {
+                Console.WriteLine("student not found exception caught\n" + s.StudentName);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Invalid Integer Number",fe.Message);
+
+                throw;
+                throw new StudentNotFoundException("Student Not Found ", "Anusha");
+            }
+            catch(OverflowException)
+            {
+                OverflowException a = new OverflowException();
+                Console.WriteLine("value you entered is too big to fit in Int32",a.Source);
+            }
+            //catch (Exception)
+            //{
+               // Console.WriteLine("some global exception has occured and we are working on it........!");
+            //}
+            finally
+            {
+                Console.WriteLine("Hope this object oriented way of programming language");
+
+            }
+            Console.WriteLine(".............................!!!!!");
+        }
+    }
+}
